16c9f858054bc43e3d8b6c67a207b8b1761d214b
Raushankumar2305 <kumarraushan1246@gmail.com>
2026-02-06 15:50:02 +0530

    Fix import case sensitivity for Homepage component

---
+++ frontend/vite-project/src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import MainComponent from "./MainComponent";
+import NavigationBar from "./NavigationBar";
+
+function homepage() {
+  return (
+    <>
+      <NavigationBar />
+      <MainComponent />
+      <Footer />
+    </>
+  );
+}
+
+export default homepage;
+
